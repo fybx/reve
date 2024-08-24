a3bb0df0815b38b162d4038eb8bf0aada8f9e0e7
Yigid BALABAN <fyb@fybx.dev>
2024-08-25 01:32:41 +0300

    update example chore

--- examples/chore_script.sh
+++ examples/chore_script.sh
@@ -11,19 +11,8 @@
 
 reve_folder="$HOME/.config/reve"
 reve_desktop_mode="$reve_folder/desktop_mode"
-gtk_dark_theme="$reve_folder/chore/gtk_dark_theme"
-gtk_light_theme="$reve_folder/chore/gtk_light_theme"
 
-util_readf () {
-    local filename=$1
-
-    if [[ -f "$filename" ]]; then
-        cat "$filename"
-    else
-        echo "util_readf: File not found: $filename" >&2
-        return 1
-    fi
-}
+source "$( reve -w )/_reve"
 
 current_mode=$( util_readf "$reve_desktop_mode" )
 echo "Hello, world! Current mode is $current_mode."
